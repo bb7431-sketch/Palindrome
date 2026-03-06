764771cffcb487d28320736b4735ab2564cfa018
B.Visshnu Perumal <bb7431@srmist.edu.in>
2026-03-06 09:19:27 +0530

    Done by B.Visshnu Perumal

---
+++ src/HotelBookingApp.java
@@ -0,0 +1,21 @@
+/**
+ * Hotel Booking Management System
+ * Application Entry Point
+ *
+ * @author Alwyn Daniels
+ * @version 1.0
+ */
+
+public class HotelBookingApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("Hotel Booking Management System");
+        System.out.println("Version 1.0");
+        System.out.println("=================================");
+        System.out.println("Welcome to the Hotel Booking Application!");
+
+    }
+
+}
